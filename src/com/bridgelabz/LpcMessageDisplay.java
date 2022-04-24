b45bb699cb21ec7778b90d1833f949aa0238b233
Jhanvi Kankhara <52102166+jhanvi006@users.noreply.github.com>
2022-04-24 06:56:35 -0400

    Delete LpcMessageDisplay.java

--- src/com/bridgelabz/LpcMessageDisplay.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz;
-
-public class LpcMessageDisplay {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program.");
-    }
-}
